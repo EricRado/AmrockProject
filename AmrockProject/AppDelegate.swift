51fc7fb99639b586946fdde924e49b49f488be59
Eric Rado <erado@axxess.com>
2020-12-13 23:14:37 -0500

    Presenting LoginViewController if the app has been inactive for a period of 1 minute

--- AmrockProject/AppDelegate.swift
+++ AmrockProject/AppDelegate.swift
@@ -9,28 +9,31 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+	private static let dateResignedKey = "DateTimeRessigned"
+	private static let dismissalTimeLimit = 60.0
 
-
+	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = LoginViewController()
+		window?.makeKeyAndVisible()
 		return true
 	}
 
-	// MARK: UISceneSession Lifecycle
-
-	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-		// Called when a new scene session is being created.
-		// Use this method to select a configuration to create the new scene with.
-		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	func applicationWillTerminate(_ application: UIApplication) {
+		UserDefaults().setValue(nil, forKey: AppDelegate.dateResignedKey)
 	}
 
-	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-		// Called when the user discards a scene session.
-		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	func applicationWillEnterForeground(_ application: UIApplication) {
+		guard let dateResigned = UserDefaults().value(forKey: AppDelegate.dateResignedKey) as? Date else { return }
+		if Date().timeIntervalSince(dateResigned) > AppDelegate.dismissalTimeLimit {
+			window?.rootViewController = LoginViewController()
+		}
 	}
 
+	func applicationWillResignActive(_ application: UIApplication) {
+		UserDefaults().setValue(Date(), forKey: AppDelegate.dateResignedKey)
+	}
 
 }
-
